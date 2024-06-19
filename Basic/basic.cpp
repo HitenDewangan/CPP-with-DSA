24fe96acfcd5f816fc260396126ef277b74d6842
Hiten <hitendewangan100j@gmail.com>
2024-06-19 23:27:26 +0530

    Added a file

---
+++ Basic/basic.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"Hello World"<<endl;
+    // cout<<"Looking For Alaska"<<endl;
+
+    int a = 10;
+    cout<<"int" << " "<<a << "    " << &a << endl;
+    
+    string name = "Alaska";
+    cout<<"string" << " "<<name << "    " << &name << endl;
+
+    bool flag = true;
+    cout<<"bool"<<" "<<flag << "    " << &flag << endl;
+
+    char c = 'a';
+    cout<<"c" << " "<<c << " " << &c << endl;
+
+    char name2 = 33;
+    cout<<"name2"<<" "<<name2 << "    " << &name2 << endl;
+
+    char name3 = '9';
+    cout<<"name3"<<" "<<name3 << "    " << &name3 << endl;
+
+
+    // char name4 = '\n';
+    // cout<<"name4"<<" "<<name4 << "    " << &name4 << endl;
+
+    char name5 = 97;
+    cout<<"name5"<<" "<<name5 << "    " << &name5 << endl;
+    // cout<<"end" << endl;
+
+    char last = 98;
+    cout<<"last"<<" "<<last << "    " << &last << endl;
+    cout<<"end" << endl;
+
+    // char name6 = "HULU";
+    // cout<<"name6"<<" "<<name6 << "    " << &name6 << endl;
+
+    
+
+}
